3f22328be1579765c2fa9e91d9337528e2d3328f
nrousseau <nrousseau@talend.com>
2016-11-03 19:23:00 +0800

    TUP-16235: organize p2 update by category

--- main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/ui/SelectExtraFeaturesToInstallWizardPage.java
+++ main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/ui/SelectExtraFeaturesToInstallWizardPage.java
@@ -16,19 +16,29 @@ import java.util.Set;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.beans.PojoObservables;
-import org.eclipse.core.databinding.beans.PojoProperties;
+import org.eclipse.core.databinding.observable.ChangeEvent;
+import org.eclipse.core.databinding.observable.IChangeListener;
+import org.eclipse.core.databinding.observable.IObservable;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.masterdetail.IObservableFactory;
+import org.eclipse.core.databinding.observable.set.IObservableSet;
 import org.eclipse.core.databinding.observable.set.ISetChangeListener;
 import org.eclipse.core.databinding.observable.set.SetChangeEvent;
+import org.eclipse.core.databinding.observable.set.WritableSet;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
-import org.eclipse.core.databinding.property.value.IValueProperty;
+import org.eclipse.equinox.internal.p2.metadata.InstallableUnit;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 import org.eclipse.jface.databinding.swt.SWTObservables;
-import org.eclipse.jface.databinding.viewers.ViewerSupport;
+import org.eclipse.jface.databinding.viewers.ObservableSetTreeContentProvider;
 import org.eclipse.jface.databinding.viewers.ViewersObservables;
 import org.eclipse.jface.databinding.wizard.WizardPageSupport;
-import org.eclipse.jface.layout.TableColumnLayout;
-import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.layout.TreeColumnLayout;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerCell;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -37,10 +47,11 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
 import org.talend.updates.runtime.i18n.Messages;
 import org.talend.updates.runtime.model.ExtraFeature;
+import org.talend.updates.runtime.model.IuP2ExtraFeature;
 
 /**
  * created by sgandon on 25 févr. 2013 Detailled comment
@@ -48,11 +59,11 @@ import org.talend.updates.runtime.model.ExtraFeature;
  */
 public class SelectExtraFeaturesToInstallWizardPage extends WizardPage {
 
-    private Table table;
+    private Tree tree;
 
     private final UpdateWizardModel updateWizardModel;
 
-    private CheckboxTableViewer checkboxTableViewer;
+    private CheckboxTreeViewer checkboxTreeViewer;
 
     private StyledText featureDescriptionText;
 
@@ -111,45 +122,116 @@ public class SelectExtraFeaturesToInstallWizardPage extends WizardPage {
 
         Composite featureComposite = new Composite(container, SWT.NONE);
         featureComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-        TableColumnLayout friendsColumnLayout = new TableColumnLayout();
+        TreeColumnLayout friendsColumnLayout = new TreeColumnLayout();
         featureComposite.setLayout(friendsColumnLayout);
 
-        checkboxTableViewer = CheckboxTableViewer.newCheckList(featureComposite, SWT.BORDER | SWT.FULL_SELECTION);
-        checkboxTableViewer.setSorter(new ViewerSorter() {// regroupt by class type and then by name
+        checkboxTreeViewer = new CheckboxTreeViewer(featureComposite, SWT.BORDER | SWT.FULL_SELECTION);
+        checkboxTreeViewer.setSorter(new ViewerSorter() {// regroupt by class type and then by name
 
-                    /*
-                     * (non-Javadoc)
-                     * 
-                     * @see org.eclipse.jface.viewers.ViewerComparator#category(java.lang.Object)
-                     */
-                    @Override
-                    public int category(Object element) {
-                        return element.getClass().hashCode();
-                    }
+            /*
+             * (non-Javadoc)
+             * 
+             * @see org.eclipse.jface.viewers.ViewerComparator#category(java.lang.Object)
+             */
+            @Override
+            public int category(Object element) {
+                return element.getClass().hashCode();
+            }
 
-                    /*
-                     * (non-Javadoc)
-                     * 
-                     * @see org.eclipse.jface.viewers.ViewerComparator#compare(org.eclipse.jface.viewers.Viewer,
-                     * java.lang.Object, java.lang.Object)
-                     */
-                    @Override
-                    public int compare(Viewer viewer, Object e1, Object e2) {
-                        if ((e2 instanceof ExtraFeature) && !((ExtraFeature) e2).mustBeInstalled()) {
-                            return -1;
-                        }
-                        return 1;
-                    }
-                });
-        table = checkboxTableViewer.getTable();
-        table.setSize(400, 155);
-        table.setHeaderVisible(true);
-        table.setLinesVisible(true);
-        TableColumn featureNameColumn = new TableColumn(table, SWT.NONE);
+            /*
+             * (non-Javadoc)
+             * 
+             * @see org.eclipse.jface.viewers.ViewerComparator#compare(org.eclipse.jface.viewers.Viewer,
+             * java.lang.Object, java.lang.Object)
+             */
+            @Override
+            public int compare(Viewer viewer, Object e1, Object e2) {
+                if ((e2 instanceof ExtraFeature) && !((ExtraFeature) e2).mustBeInstalled()) {
+                    return -1;
+                }
+                return 1;
+            }
+        });
+        tree = checkboxTreeViewer.getTree();
+        tree.setSize(400, 155);
+        tree.setHeaderVisible(true);
+        tree.setLinesVisible(true);
+        final Realm realm = SWTObservables.getRealm(checkboxTreeViewer.getControl().getDisplay());
+
+        (updateWizardModel.availableExtraFeatures).addChangeListener(new IChangeListener() {
+            
+            @Override
+            public void handleChange(ChangeEvent event) {
+                System.out.println();
+            }
+        });
+        final IObservableFactory setFactory = new IObservableFactory() {
+            public IObservable createObservable(final Object target) {
+                if (target instanceof WritableSet) {
+                    return (IObservableSet) target;
+                }
+                InstallableUnit iu = new InstallableUnit();
+                iu.setProperty(IInstallableUnit.PROP_NAME, "test");
+                IuP2ExtraFeature p2e = new IuP2ExtraFeature(iu,"abc");
+                
+//                Set<ExtraFeature> features = new HashSet<>();
+//                features.add(p2e);
+                WritableSet set = new WritableSet(realm);
+                set.add(p2e);
+                return set;
+//                if (target instanceof Object[]) {
+//                    return Observables.staticObservableList(realm, Arrays.asList((Object[]) target));
+//                }
+//                Object value;
+//                if (target instanceof FakeRoot) {
+//                    value = ((FakeRoot) target).getRoot();
+//                    if (value == null) {
+//                        return new ObservableList(Collections.EMPTY_LIST, treeElementClass) {
+//                            // empty list
+//                        };
+//                    }
+//                } else {
+//                    value = target;
+//                }
+//                if (AbstractSWTWidgetRidget.isBean(treeElementClass)) {
+//                    return BeansObservables.observeList(realm, value, childrenAccessor, treeElementClass);
+//                } else {
+//                    return PojoObservables.observeList(realm, value, childrenAccessor, treeElementClass);
+//                }
+            }
+        };
+
+        // Label provider for the tree
+        IBaseLabelProvider labelProvider = new CellLabelProvider() {
+
+            @Override
+            public void update(ViewerCell cell) {
+                if (cell.getColumnIndex() == 0) {
+                    cell.setText(((ExtraFeature)cell.getElement()).getName());
+                } else {
+                    cell.setText(((ExtraFeature)cell.getElement()).getVersion());
+                }
+            }
+            
+        };
+        
+     // UpdatableTreeContentProvider converts an ITreeProvider into a
+        // standard JFace content provider
+        ObservableSetTreeContentProvider contentProvider = new ObservableSetTreeContentProvider(
+                setFactory, null);
+
+        checkboxTreeViewer.setContentProvider(contentProvider);
+        checkboxTreeViewer.setLabelProvider(labelProvider);
+
+        // For the ITreeProvider above, it doesn't matter what we select as the
+        // input.
+        checkboxTreeViewer.setInput(updateWizardModel.availableExtraFeatures);
+        
+        TreeColumn featureNameColumn = new TreeColumn(tree, SWT.NONE);
         featureNameColumn.setText(Messages.getString("SelectExtraFeaturesToInstallWizardPage.feature.column.name.name")); //$NON-NLS-1$
         friendsColumnLayout.setColumnData(featureNameColumn, new ColumnWeightData(3, true));
 
-        TableColumn tblclmnVersion = new TableColumn(table, SWT.NONE);
+        TreeColumn tblclmnVersion = new TreeColumn(tree, SWT.NONE);
         friendsColumnLayout.setColumnData(tblclmnVersion, new ColumnWeightData(1, true));
         tblclmnVersion.setText(Messages.getString("SelectExtraFeaturesToInstallWizardPage.features.column.version.name")); //$NON-NLS-1$
 
@@ -168,16 +250,27 @@ public class SelectExtraFeaturesToInstallWizardPage extends WizardPage {
 
     protected DataBindingContext initDataBindings() {
         dbc = new DataBindingContext();
-        // bind the table elements with the updateWizardModel availableExtraFeatures and the columns with the
-        // extrafeatures values
-        ViewerSupport.bind(checkboxTableViewer, updateWizardModel.availableExtraFeatures,
-                new IValueProperty[] { PojoProperties.value(ExtraFeature.class, "name"), //$NON-NLS-1$
-                        PojoProperties.value(ExtraFeature.class, "version") }); //$NON-NLS-1$
+//        IListProperty childrenProp = new DelegatingListProperty() {
+//
+//            protected IListProperty doGetDelegate(Object source) {
+//                if (source instanceof ExtraFeature) {
+//                    return null;
+//                }
+//                IListProperty f = BeanProperties.list(RootFeature.class, "children");
+//                return categories;
+//            }
+//
+//        };
+//        // bind the table elements with the updateWizardModel availableExtraFeatures and the columns with the
+//        // extrafeatures values
+//        ViewerSupport.bind(checkboxTreeViewer, updateWizardModel.availableExtraFeatures, childrenProp,
+//                new IValueProperty[] { PojoProperties.value(ExtraFeature.class, "name"), //$NON-NLS-1$
+//                        PojoProperties.value(ExtraFeature.class, "version") }); //$NON-NLS-1$
         // bind selecting of the check boxes to the selected extra features set in the model
-        dbc.bindSet(ViewersObservables.observeCheckedElements(checkboxTableViewer, ExtraFeature.class),
+        dbc.bindSet(ViewersObservables.observeCheckedElements(checkboxTreeViewer, ExtraFeature.class),
                 updateWizardModel.selectedExtraFeatures);
         // bind the table selection desctiption to the text field
-        IObservableValue selectedFeature = ViewersObservables.observeSingleSelection(checkboxTableViewer);
+        IObservableValue selectedFeature = ViewersObservables.observeSingleSelection(checkboxTreeViewer);
         dbc.bindValue(SWTObservables.observeText(featureDescriptionText),
                 PojoObservables.observeDetailValue(selectedFeature, "description", String.class)); //$NON-NLS-1$
         // add a validator for feature selection because SetObservable does not provide any validator.
@@ -207,7 +300,7 @@ public class SelectExtraFeaturesToInstallWizardPage extends WizardPage {
         for (ExtraFeature ef : features) {
             if (ef.mustBeInstalled()) {
                 updateWizardModel.selectedExtraFeatures.add(ef);
-            }// else do not select caus must not be installed
+            } // else do not select caus must not be installed
         }
     }
 
--- main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/ui/UpdateWizardModel.java
+++ main/plugins/org.talend.updates.runtime/src/main/java/org/talend/updates/runtime/ui/UpdateWizardModel.java
@@ -15,9 +15,13 @@ package org.talend.updates.runtime.ui;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+import org.eclipse.core.databinding.observable.set.ComputedSet;
 import org.eclipse.core.databinding.observable.set.IObservableSet;
 import org.eclipse.core.databinding.observable.set.WritableSet;
 import org.eclipse.core.databinding.validation.IValidator;
@@ -44,8 +48,8 @@ public class UpdateWizardModel {
         @Override
         protected IStatus validate() {
             if (selectedExtraFeatures.isEmpty()) {
-                return ValidationStatus.error(Messages
-                        .getString("SelectExtraFeaturesToInstallWizardPage.one.feature.must.be.selected")); //$NON-NLS-1$
+                return ValidationStatus
+                        .error(Messages.getString("SelectExtraFeaturesToInstallWizardPage.one.feature.must.be.selected")); //$NON-NLS-1$
             } else if (!selectedExtraFeatures.isEmpty() && !canConfigureUpdateSiteLocation()) {
                 @SuppressWarnings("unchecked")
                 ExtraFeature extraFeature = getFirstExtraFeatureNotAllowingUpdateSiteConfig(selectedExtraFeatures);
@@ -108,7 +112,7 @@ public class UpdateWizardModel {
      */
     public UpdateWizardModel(Set<ExtraFeature> extraFeatures) {
         // create an observable set of the feature that may be installed
-        availableExtraFeatures = extraFeatures != null ? new WritableSet(extraFeatures, ExtraFeature.class) : new WritableSet();
+        availableExtraFeatures = extraFeatures != null ? new FilteredCategories(extraFeatures, ExtraFeature.class) : new FilteredCategories();;
         this.featureRepositories = new FeatureRepositories();
     }
 
@@ -155,7 +159,8 @@ public class UpdateWizardModel {
      */
     public boolean canConfigureUpdateSiteLocation() {
         @SuppressWarnings("unchecked")
-        ExtraFeature firstExtraFeatureNotAllowingUpdateSiteConfig = getFirstExtraFeatureNotAllowingUpdateSiteConfig(selectedExtraFeatures);
+        ExtraFeature firstExtraFeatureNotAllowingUpdateSiteConfig = getFirstExtraFeatureNotAllowingUpdateSiteConfig(
+                selectedExtraFeatures);
         return firstExtraFeatureNotAllowingUpdateSiteConfig == null;
     }
 
@@ -166,11 +171,42 @@ public class UpdateWizardModel {
     private ExtraFeature getFirstExtraFeatureNotAllowingUpdateSiteConfig(Set<ExtraFeature> extraFeatures) {
         for (ExtraFeature ef : extraFeatures) {
             EnumSet<UpdateSiteLocationType> updateSiteCompatibleTypes = ef.getUpdateSiteCompatibleTypes();
-            if (updateSiteCompatibleTypes.size() == 1 && updateSiteCompatibleTypes.contains(UpdateSiteLocationType.DEFAULT_REPO)) {
+            if (updateSiteCompatibleTypes.size() == 1
+                    && updateSiteCompatibleTypes.contains(UpdateSiteLocationType.DEFAULT_REPO)) {
                 return ef;
             }
         }
         return null;
     }
 
+    private class FilteredCategories extends WritableSet {
+
+
+        /**
+         * DOC nrousseau FilteredCategories constructor comment.
+         * @param extraFeatures
+         * @param class1
+         */
+        public FilteredCategories(Set<ExtraFeature> extraFeatures, Class<ExtraFeature> class1) {
+            super(extraFeatures, class1);
+        }
+
+        /**
+         * DOC nrousseau FilteredCategories constructor comment.
+         */
+        public FilteredCategories() {
+            super();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see org.eclipse.core.databinding.observable.set.AbstractObservableSet#add(java.lang.Object)
+         */
+        @Override
+        public boolean add(Object o) {
+            return super.add(o);
+        }
+
+    }
 }
