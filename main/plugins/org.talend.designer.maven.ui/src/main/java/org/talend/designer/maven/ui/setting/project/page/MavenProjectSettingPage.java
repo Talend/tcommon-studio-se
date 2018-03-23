c8d81b5dedaab6934dc951a00f0ca8d0c165c77e
jding-tlnd <jding@talend.com>
2018-03-23 19:34:42 +0800

    feat(TUP-19782)Add filter on the pom synchronization https://jira.talendforge.org/browse/TUP-19782

--- main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/project/page/MavenProjectSettingPage.java
+++ main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/project/page/MavenProjectSettingPage.java
@@ -12,14 +12,25 @@
 // ============================================================================
 package org.talend.designer.maven.ui.setting.project.page;
 
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.GlobalServiceRegister;
+import org.talend.core.runtime.maven.MavenConstants;
 import org.talend.core.runtime.projectsetting.AbstractProjectSettingPage;
+import org.talend.core.runtime.services.IFilterService;
+import org.talend.designer.maven.DesignerMavenPlugin;
 import org.talend.designer.maven.tools.AggregatorPomsHelper;
 import org.talend.designer.maven.ui.i18n.Messages;
 
@@ -27,9 +38,18 @@ import org.talend.designer.maven.ui.i18n.Messages;
  * DOC ggu class global comment. Detailled comment
  */
 public class MavenProjectSettingPage extends AbstractProjectSettingPage {
+	
+	private Text filterText;
+	
+	private IPreferenceStore preferenceStore;
 
     public MavenProjectSettingPage() {
         noDefaultAndApplyButton();
+    }
+    
+    @Override
+    protected String getPreferenceName() {
+        return DesignerMavenPlugin.PLUGIN_ID;
     }
 
     @Override
@@ -38,6 +58,25 @@ public class MavenProjectSettingPage extends AbstractProjectSettingPage {
         parent.setLayout(new GridLayout());
         Button button = new Button(parent, SWT.NONE);
         button.setText(Messages.getString("ProjectPomProjectSettingPage.syncAllPomsButtonText")); //$NON-NLS-1$
+        
+        preferenceStore = getPreferenceStore();
+        Label filterLabel = new Label(parent, SWT.NONE);
+        filterLabel.setText(Messages.getString("ProjectPomProjectSettingPage_FilterPomLabel"));
+        filterText = new Text(parent, SWT.BORDER);
+        filterText.setLayoutData(new GridData(SWT.FILL,SWT.CENTER,true,false));
+        if(StringUtils.isBlank(preferenceStore.getString(MavenConstants.POM_FILTER))) {
+        	filterText.setText("");
+        }else {
+        	filterText.setText(preferenceStore.getString(MavenConstants.POM_FILTER));
+        }
+        filterText.addModifyListener(new ModifyListener() {
+			
+			@Override
+			public void modifyText(ModifyEvent e) {
+				preferenceStore.setValue(MavenConstants.POM_FILTER, filterText.getText());
+			}
+		});
+        
         button.addSelectionListener(new SelectionAdapter() {
 
             @Override
@@ -46,10 +85,37 @@ public class MavenProjectSettingPage extends AbstractProjectSettingPage {
                     new AggregatorPomsHelper().syncAllPoms();
                 } catch (Exception e) {
                     ExceptionHandler.process(e);
+                    if("filter_parse_error".equals(e.getMessage())) {
+                    	setErrorMessage(Messages.getString("ProjectPomProjectSettingPage_FilterErrorMessage"));
+                    }
                 }
             }
 
         });
+        
+        
     }
+
+	@Override
+	public boolean performOk() {
+		super.performOk();
+		try {
+			if(GlobalServiceRegister.getDefault().isServiceRegistered(IFilterService.class)) {
+				IFilterService service = (IFilterService) GlobalServiceRegister.getDefault().getService(IFilterService.class);
+				service.checkFilterContent(filterText.getText());
+			}else {
+				setErrorMessage(Messages.getString("ProjectPomProjectSettingPage_FilterServiceErrorMessage"));
+			}
+		} catch (Exception e) {
+			ExceptionHandler.process(e);
+            if("filter_parse_error".equals(e.getMessage())) {
+            	setErrorMessage(Messages.getString("ProjectPomProjectSettingPage_FilterErrorMessage"));
+            }
+            return false;
+		}
+		return true;
+	}
+    
+    
 
 }
