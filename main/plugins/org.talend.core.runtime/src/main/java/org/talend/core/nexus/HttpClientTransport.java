bf5bc166469642d976f35ba6d92f00af45bcd3b4
Chao MENG <cmeng@talend.com>
2018-03-29 20:04:28 +0800

    fix(TUP-19667): use HTTP proxy instead of SOCKS https://jira.talendforge.org/browse/TUP-19667

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/nexus/HttpClientTransport.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/nexus/HttpClientTransport.java
@@ -12,15 +12,26 @@
 // ============================================================================
 package org.talend.core.nexus;
 
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.PasswordAuthentication;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ProxySelector;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -33,6 +44,8 @@ import org.talend.core.runtime.maven.MavenUrlHelper;
  * DOC ggu class global comment. Detailled comment
  */
 public abstract class HttpClientTransport {
+
+    private static final String PROP_PROXY_HTTP_CLIENT_USE_DEFAULT_SETTINGS = "talend.proxy.HttpClient.useDefaultSettings"; //$NON-NLS-1$
 
     private String baseURI;
 
@@ -80,11 +93,24 @@ public abstract class HttpClientTransport {
             return;
         }
         DefaultHttpClient httpClient = new DefaultHttpClient();
+
+        /**
+         * Normally the default proxySelector is EclipseProxySelector
+         */
+        final ProxySelector proxySelector = AccessController.doPrivileged(new PrivilegedAction<ProxySelector>() {
+
+            @Override
+            public ProxySelector run() {
+                return ProxySelector.getDefault();
+            }
+        });
         try {
             if (StringUtils.isNotBlank(username)) { // set username
                 httpClient.getCredentialsProvider().setCredentials(new AuthScope(requestURI.getHost(), requestURI.getPort()),
                         new UsernamePasswordCredentials(username, password));
             }
+
+            configureProxy(httpClient, requestURI);
             HttpResponse response = execute(monitor, httpClient, requestURI);
 
             processResponseCode(response);
@@ -95,6 +121,65 @@ public abstract class HttpClientTransport {
             throw new Exception(requestURI.toString(), e);
         } finally {
             httpClient.getConnectionManager().shutdown();
+            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+
+                @Override
+                public Void run() {
+                    ProxySelector.setDefault(proxySelector);
+                    return null;
+                }
+            });
+        }
+    }
+
+    private void configureProxy(final DefaultHttpClient httpClient, URI requestURI) {
+        if (Boolean.valueOf(System.getProperty(PROP_PROXY_HTTP_CLIENT_USE_DEFAULT_SETTINGS, Boolean.FALSE.toString()))) {
+            return;
+        }
+        /**
+         * Get proxy configured in Eclipse preference
+         */
+        ProxySelector proxySelector = AccessController.doPrivileged(new PrivilegedAction<ProxySelector>() {
+
+            @Override
+            public ProxySelector run() {
+                return ProxySelector.getDefault();
+            }
+        });
+        final List<Proxy> proxyList = proxySelector.select(requestURI);
+        Proxy usedProxy = null;
+        if (proxyList != null && !proxyList.isEmpty()) {
+            usedProxy = proxyList.get(0);
+        }
+
+        if (usedProxy != null) {
+            if (Type.DIRECT.equals(usedProxy.type())) {
+                return;
+            }
+            final Proxy finalProxy = usedProxy;
+            InetSocketAddress address = (InetSocketAddress) finalProxy.address();
+            PasswordAuthentication proxyAuthentication = Authenticator.requestPasswordAuthentication(address.getHostName(),
+                    address.getAddress(), address.getPort(), "Http Proxy", "Http proxy authentication", null);
+            String proxyUser = proxyAuthentication.getUserName();
+            String proxyPassword = new String(proxyAuthentication.getPassword());
+            String proxyServer = address.getHostName();
+            int proxyPort = address.getPort();
+            httpClient.getCredentialsProvider().setCredentials(new AuthScope(proxyServer, proxyPort),
+                    new UsernamePasswordCredentials(proxyUser, proxyPassword));
+            HttpHost proxyHost = new HttpHost(proxyServer, proxyPort);
+            httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxyHost);
+
+            /**
+             * set default proxySelector to null, since the EclipseProxySelector will use SOCKS proxy instead of HTTP
+             */
+            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+
+                @Override
+                public Void run() {
+                    ProxySelector.setDefault(null);
+                    return null;
+                }
+            });
         }
     }
 
