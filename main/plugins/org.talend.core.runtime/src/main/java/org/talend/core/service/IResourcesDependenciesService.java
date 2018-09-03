14b41476c93d539bc1773e6aac46bdfd3fbc8ae9
jding-tlnd <jding@talend.com>
2018-09-03 18:29:21 +0800

    fix(TUP-20630)[BUG] Resource job throw FileNotFoundException if moved the location https://jira.talendforge.org/browse/TUP-20630 Set the resource context value shorter like(folder/test.txt),Change the resource runtime context value to resource dependency path.

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IResourcesDependenciesService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IResourcesDependenciesService.java
@@ -1,10 +1,13 @@
 package org.talend.core.service;
 
 import org.talend.core.IService;
+import org.talend.core.model.process.IProcess;
 import org.talend.core.model.repository.IRepositoryViewObject;
 
 public interface IResourcesDependenciesService extends IService {
 
     public void copyToExtResourceFolder(IRepositoryViewObject repoObject, String jobLabel, String version);
+
+    public String getResourcePathForContext(IProcess process, String contextName);
 
 }
