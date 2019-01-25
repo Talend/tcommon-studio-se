54c6b4bcb4c2fa981e983d176b5b5cfcdeebfe6e
Tao Tao <ttao@talend.com>
2019-01-25 17:04:49 +0800

    workItem(TUP-21143):Generate random value for null password hiding code
    https://jira.talendforge.org/browse/TUP-21143

--- test/plugins/org.talend.core.test/src/org/talend/core/model/utils/TalendTextUtilsTest.java
+++ test/plugins/org.talend.core.test/src/org/talend/core/model/utils/TalendTextUtilsTest.java
@@ -8,39 +8,50 @@ public class TalendTextUtilsTest {
 
     @Test
     public void testHidePassword(){
-        String pass = "((String) globalMap(\"myPasswd\"))";
+        String pass = "(( String) globalMap(\"myPasswd\"))";
         String result = TalendTextUtils.hidePassword(pass);
         assertEquals(result,pass);
-        
+
         String temp = "ccc";
         pass = "\"aaa\""+temp+"\"bbb\"";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result,pass);
-        
+
         pass = " value ";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
         pass = " va  lue ";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
         pass = null;
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
         pass = " ";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
         pass = "\"\"";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
         pass = "\"  \"";
         result = TalendTextUtils.hidePassword(pass);
         assertEquals(result, "********");
-        
+
+        pass = "context.value";
+        result = TalendTextUtils.hidePassword(pass);
+        assertEquals(result, pass);
+
+        pass = "context. value";
+        result = TalendTextUtils.hidePassword(pass);
+        assertEquals(result, "********");
+
+        pass = "con text. value";
+        result = TalendTextUtils.hidePassword(pass);
+        assertEquals(result, "********");
     }
 
     @Test
