309b9742b0d132a55cb2dc4af6d93ea59eaa1443
Kevin Wang <kjwang@talend.com>
2019-05-24 15:31:39 +0800

    TUP-21489 Remove use of MD5 class in the Studio https://jira.talendforge.org/browse/TUP-21489

--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/string/DigestUtil.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/string/DigestUtil.java
@@ -1,8 +1,6 @@
-package org.talend.utils.string;
-
 //============================================================================
 //
-//Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//Copyright (C) 2006-2019 Talend Inc. - www.talend.com
 //
 //This source code is available under agreement available at
 //%InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -12,6 +10,8 @@ package org.talend.utils.string;
 //9 rue Pages 92150 Suresnes, France
 //
 //============================================================================
+package org.talend.utils.string;
+
 public class DigestUtil {
 
 	public static String sha256Hex(final byte[] data) {
