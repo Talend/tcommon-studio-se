2cf4ba789066f6ec59ee52c9632b99998a66c1ef
zhangjie <jie.zhang@talend.com>
2019-05-29 19:17:08 +0800

    fix(TUP-21179)Studio doesn't commit "Route Resources" to SVN

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/RepositoryWorkUnit.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/repository/RepositoryWorkUnit.java
@@ -45,6 +45,8 @@ public abstract class RepositoryWorkUnit<T> {
 
     private boolean refreshRepository = true; // added for TDI-27085
 
+    private boolean svnFileModefied = false;
+
     /**
      * Usefull for some save only actions, where we're sure everything is up to date.
      */
@@ -211,4 +213,13 @@ public abstract class RepositoryWorkUnit<T> {
     public void setAvoidUpdateLocks(boolean avoidUpdateLocks) {
         this.avoidUpdateLocks = avoidUpdateLocks;
     }
+
+    public boolean isSvnFileModefied() {
+        return svnFileModefied;
+    }
+
+    public void setSvnFileModefied(boolean svnFileModefied) {
+        this.svnFileModefied = svnFileModefied;
+    }
+
 }
