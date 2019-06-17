696e6eccc90cf2edb15653e6167bf85af399b902
jding-tlnd <jding@talend.com>
2019-06-17 14:34:35 +0800

    fix(TUP-23482)should avoid multi version slf4j-log4j jars in the job classpath https://jira.talendforge.org/browse/TUP-23482

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/utils/MavenVersionHelper.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/utils/MavenVersionHelper.java
@@ -13,8 +13,11 @@
 package org.talend.designer.maven.utils;
 
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
+import org.talend.core.runtime.maven.MavenConstants;
 
 public class MavenVersionHelper {
+
+    private static final String SEGMENT = "-";
 
     /**
      * compare maven artifact version
@@ -23,6 +26,15 @@ public class MavenVersionHelper {
         DefaultArtifactVersion version = new DefaultArtifactVersion(versionStr);
         DefaultArtifactVersion otherVersion = new DefaultArtifactVersion(otherVersionStr);
         return version.compareTo(otherVersion);
+    }
+
+    public static String getJarOriginalVersion(String jarName) {
+        String artifactId = jarName;
+        if (jarName.endsWith(MavenConstants.TYPE_JAR)) {
+            artifactId = jarName.substring(0, jarName.lastIndexOf(MavenConstants.TYPE_JAR) - 1);
+        }
+        String version = artifactId.substring(artifactId.lastIndexOf(SEGMENT) + 1);
+        return version;
     }
 
 }
