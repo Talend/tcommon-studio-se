da4b503078e9b3521b7a597602dfa8d0178cdf83
wwang-talend <wwang@talend.com>
2019-09-10 14:47:23 +0800

    fix(TDI-42187): Fix Double Checked Locking in GlobalResource

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/GlobalResource.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/GlobalResource.java
@@ -14,28 +14,31 @@ package routines.system;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GlobalResource {
 
-    // let it support the top level Object
-    public static Map<Object, Object> resourceMap = new HashMap<Object, Object>();
+	// let it support the top level Object
+	public static Map<Object, Object> resourceMap = new HashMap<Object, Object>();
 
-    // when there is multiple threads wants to insert stats&logs&meta into DB, it is used as a locker. bug:22677
-    public static TalendMultiThreadLockMap resourceLockMap = new TalendMultiThreadLockMap();
+	// when there is multiple threads wants to insert stats&logs&meta into DB, it is
+	// used as a locker. bug:22677
+	public static TalendMultiThreadLockMap resourceLockMap = new TalendMultiThreadLockMap();
 
-    public static class TalendMultiThreadLockMap {
+	public static class TalendMultiThreadLockMap {
 
-        private Map<Object, Object> tMultiTheadLockMap = new HashMap<Object, Object>();
+		private Map<Object, Object> tMultiTheadLockMap = new ConcurrentHashMap<>();
 
-        public Object get(Object key) {
-            if (tMultiTheadLockMap.get(key) == null) {
-                synchronized (TalendMultiThreadLockMap.this) {
-                    if (tMultiTheadLockMap.get(key) == null) {
-                        tMultiTheadLockMap.put(key, new Object());
-                    }
-                }
-            }
-            return tMultiTheadLockMap.get(key);
-        }
-    }
+		public Object get(Object key) {
+			Object value = tMultiTheadLockMap.get(key);
+			if (value == null) {
+				value = new Object();
+				Object storedValue = tMultiTheadLockMap.putIfAbsent(key, value);
+				if (storedValue != null) {
+					value = storedValue;
+				}
+			}
+			return value;
+		}
+	}
 }
