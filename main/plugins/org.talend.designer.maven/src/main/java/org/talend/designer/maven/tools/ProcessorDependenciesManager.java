175c4b6a6ffa7c346bb1e179fbb2a289b068fad7
zhangjie <jie.zhang@talend.com>
2019-12-19 19:10:57 +0800

    advoid loop method

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/ProcessorDependenciesManager.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/ProcessorDependenciesManager.java
@@ -167,7 +167,7 @@ public class ProcessorDependenciesManager {
         if (modulesNeeded.isEmpty()) {
             modulesNeeded = processor.getNeededModules(TalendProcessOptionConstants.MODULES_WITH_JOBLET);
         }
-        processor.updateModulesAfterSetLog4j(modulesNeeded);
+        // processor.updateModulesAfterSetLog4j(modulesNeeded);
         neededLibraries.addAll(modulesNeeded);
 
         // add testcase modules
