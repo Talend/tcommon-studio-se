cce2ac7058c6718ebcc96337e67bbd6cbf10d6de
bhe-talendbj <bhe@talend.com>
2020-01-13 18:02:25 +0800

    feat(TUP-25829) Remove unused imports

--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/security/StudioEncryption.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/security/StudioEncryption.java
@@ -14,7 +14,6 @@ package org.talend.utils.security;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
