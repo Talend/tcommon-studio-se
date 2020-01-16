94c58514fefc54cb9989952fb250d00d71312897
bhe-talendbj <bhe@talend.com>
2020-01-16 11:33:54 +0800

    feat(TUP-25829) Resolve comments

--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/security/StudioEncryption.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/security/StudioEncryption.java
@@ -224,8 +224,8 @@ public class StudioEncryption {
                     // key file exists, check whether to generate custom encryption keys
                     try {
                         if (generateEncryptionKeys(keyFile)) {
-                            LOGGER.info("Customized encryption keys generated, please synchronize key file" + keyFile
-                                    + " to TAC and Jobserver");
+                            LOGGER.info("Customized encryption keys generated, please synchronize key file " + keyFile
+                                    + " to Administrator and Jobserver");
                         }
                     } catch (Exception e) {
                         LOGGER.error("Generate customized encryption keys error", e);
