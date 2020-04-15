b8e778b23a887e689b25a53cb500d40d1bef8f9d
hwang <hwang@talend.com>
2020-04-15 19:29:37 +0800

    bugfix(TUP-26875):when import a job with many versions,the order of the version is not right

--- main/plugins/org.talend.repository.items.importexport.ui/src/main/java/org/talend/repository/items/importexport/ui/wizard/imports/providers/ImportItemsViewerSorter.java
+++ main/plugins/org.talend.repository.items.importexport.ui/src/main/java/org/talend/repository/items/importexport/ui/wizard/imports/providers/ImportItemsViewerSorter.java
@@ -15,6 +15,7 @@ package org.talend.repository.items.importexport.ui.wizard.imports.providers;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.talend.repository.items.importexport.wizard.models.FolderImportNode;
+import org.talend.repository.items.importexport.wizard.models.ImportNode;
 import org.talend.repository.items.importexport.wizard.models.ItemImportNode;
 import org.talend.repository.items.importexport.wizard.models.ProjectImportNode;
 import org.talend.repository.items.importexport.wizard.models.TypeImportNode;
@@ -62,6 +63,43 @@ public class ImportItemsViewerSorter extends ViewerSorter {
         // return -1;
         //
         // }
+    	if(o1 instanceof ImportNode && o2 instanceof ImportNode) {
+
+    		if(((ImportNode)o1).getItemRecord() != null && ((ImportNode)o2).getItemRecord() != null 
+        			&& ((ImportNode)o1).getItemRecord().getProperty() != null 
+        			&& ((ImportNode)o2).getItemRecord().getProperty() != null) {
+        		String label1 = ((ImportNode)o1).getItemRecord().getProperty().getLabel();
+                String label2 = ((ImportNode)o2).getItemRecord().getProperty().getLabel();
+                if (label1 == null) {
+                    return -1;
+                }
+                if (label2 == null) {
+                    return 1;
+                }
+                if(label1.equals(label2)) {
+                	String version1 = ((ImportNode)o1).getItemRecord().getProperty().getVersion();
+                	String version2 = ((ImportNode)o2).getItemRecord().getProperty().getVersion();
+                	
+                	if (version1 == null) {
+                        return -1;
+                    }
+                    if (version2 == null) {
+                        return 1;
+                    }
+                    try {
+                		float v1 = Float.parseFloat(version1);
+                        float v2 = Float.parseFloat(version2);
+                        if(v1 > v2) {
+                        	return 1;
+                        }else if(v1 < v2) {
+                        	return -1;
+                        }
+                	} catch (Exception e) {
+                		return 0;
+                	}
+                }
+            }
+    	}
         return super.compare(viewer, o1, o2);
     }
 
