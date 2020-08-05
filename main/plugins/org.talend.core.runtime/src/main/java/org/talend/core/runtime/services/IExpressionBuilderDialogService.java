727b5afedc44718b0fc5b69ea8d6b498f670d1be
hzhao-talendbj <hzhao@talend.com>
2020-08-05 12:21:28 +0800

    TUP-25103

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/services/IExpressionBuilderDialogService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/services/IExpressionBuilderDialogService.java
@@ -37,4 +37,7 @@ public interface IExpressionBuilderDialogService extends IService {
 
     public IExpressionBuilderDialogController getExpressionBuilderInstance(Composite parent, IExpressionDataBean dataBean,
             INode component, boolean isBatch);
+
+    public IExpressionBuilderDialogController getExpressionBuilderInstance(Composite parent, IExpressionDataBean dataBean,
+            INode component, int ExpressionBuilderType);
 }
