1eade5276cb019aab6f101ed88ac3d81005b04d8
bhe-talendbj <bhe@talend.com>
2020-08-07 17:30:46 +0800

    fix(TUP-26346): Fix switch nexus search hanlder

--- main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/nexus/Nexus3RepositoryHandler.java
+++ main/plugins/org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/nexus/Nexus3RepositoryHandler.java
@@ -58,8 +58,6 @@ public class Nexus3RepositoryHandler extends AbstractArtifactRepositoryHandler {
 
     private static final ConcurrentHashMap<ArtifactRepositoryBean, INexus3SearchHandler> LAST_HANDLER_MAP = new ConcurrentHashMap<ArtifactRepositoryBean, INexus3SearchHandler>();
     
-    private static List<INexus3SearchHandler> queryHandlerList = new ArrayList<INexus3SearchHandler>();
-
     @Override
     public IRepositoryArtifactHandler clone() {
         return new Nexus3RepositoryHandler();
@@ -159,7 +157,7 @@ public class Nexus3RepositoryHandler extends AbstractArtifactRepositoryHandler {
         try {
             result = currentQueryHandler.search(repositoryId, groupIdToSearch, artifactId, versionToSearch);
         } catch (Exception ex) {
-            for (int i = 0; i < queryHandlerList.size(); i++) {// Try to other version
+            for (int i = 0; i < 3; i++) {// Try to other version
                 INexus3SearchHandler handler = createQueryHandler(i);
                 if (handler != currentQueryHandler) {
                     try {
