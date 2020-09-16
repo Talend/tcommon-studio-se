aaaf4e5eac716fe51a50908bb84d473ba3050d47
bhe-talendbj <bhe@talend.com>
2020-09-16 16:22:22 +0800

    feat(TUP-28666): Extract TalendTextUtils

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
@@ -490,40 +490,7 @@ public class TalendTextUtils {
     }
 
     public static String trimParameter(String value) {
-        if (value == null) {
-            return null;
-        }
-        int length = value.length();
-        String result = removeQuotes(value);
-        if (length > 1
-                && (((value.startsWith("\"") && value.endsWith("\""))) || (value.startsWith("\'") && value.endsWith("\'")))) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-            result = value.substring(1, length - 1);
-
-            if (result.contains("\\")) { //$NON-NLS-1$
-                result = result.replaceAll("\\\\n", "\n"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\b", "\b"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\f", "\f"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\r", "\r"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\t", "\t"); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\\"", "\""); //$NON-NLS-1$ //$NON-NLS-2$
-                result = result.replaceAll("\\\\\\\\", "\\\\"); //$NON-NLS-1$ //$NON-NLS-2$
-
-            	// handle unicode
-                if (result.contains("\\u")) {
-                	for (int indexStart = 0; result.indexOf("\\u", indexStart) >= 0; indexStart = result.indexOf("\\u", indexStart)) {
-                		if (result.indexOf("\\u", indexStart) + 5 <= result.length()) { //$NON-NLS-1$
-                			int unicodeStart = result.indexOf("\\u"); //$NON-NLS-1$
-                			int unicodeEnd = unicodeStart + 5;
-                			result = result.substring(0, Math.max(0, unicodeStart))
-                					+ StringEscapeUtils.unescapeJava(result.substring(unicodeStart, unicodeEnd + 1))
-                					+ result.substring(Math.min(unicodeEnd + 1, result.length()), result.length());
-                		}
-	                }
-                }
-            }
-        }
-
-        return result;
+        return org.talend.utils.string.TalendTextUtils.trimParameter(value);
     }
 
     public static String getQuoteChar() {
