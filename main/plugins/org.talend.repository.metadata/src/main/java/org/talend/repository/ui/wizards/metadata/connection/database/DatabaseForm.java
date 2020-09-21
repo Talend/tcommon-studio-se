acd60fb53925f7cd0ad531fce9b49ac731050e63
bhe-talendbj <bhe@talend.com>
2020-09-21 11:43:10 +0800

    feat(TUP-25246): Update metadata driver jar

--- main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
+++ main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
@@ -5736,7 +5736,7 @@ public class DatabaseForm extends AbstractForm {
                             .getService(ILibraryManagerUIService.class);
                     IConfigModuleDialog dialog = libUiService.getConfigModuleDialog(getShell(), null);
                     if (dialog.open() == IDialogConstants.OK_ID) {
-                        String selecteModule = dialog.getModuleName();
+                        String selecteModule = dialog.getMavenURI();
                         if (selecteModule != null && !asList.contains(selecteModule)) {
                             asList.add(selecteModule);
                         }
