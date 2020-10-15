17ba51e123ca62fd4cc2ba53d3c079c5cc1ac827
sbliu <sbliu@talend.com>
2020-10-15 16:30:50 +0800

    TUP-28944 fix the unit test.

--- test/plugins/org.talend.commons.runtime.test/src/org/talend/commons/utils/time/PerformanceStatisticUtilTest.java
+++ test/plugins/org.talend.commons.runtime.test/src/org/talend/commons/utils/time/PerformanceStatisticUtilTest.java
@@ -14,8 +14,6 @@ package org.talend.commons.utils.time;
 
 import static org.junit.Assert.assertEquals;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Locale;
 
 import org.junit.Test;
@@ -29,36 +27,18 @@ public class PerformanceStatisticUtilTest {
     public void testNumberFormat() {
         Locale defaultLocale = Locale.getDefault();
         double dvalue = 123456123456.789123456789;
-        String pattern = "#,###.##";
         try {
             Locale.setDefault(new Locale("en","US"));
-            DecimalFormat df = new DecimalFormat(pattern);
-            assertEquals("123,456,123,456.79",df.format(dvalue));
+            assertEquals(new Locale("en","US"), Locale.getDefault());
             assertEquals("123456123456.79",PerformanceStatisticUtil.format(dvalue));
             
             Locale.setDefault(new Locale("fr","FR"));
-            // DecimalFormat need to create after defaultLocale settle to apply
-            df = new DecimalFormat(pattern);
-            assertEquals("123 456 123 456,79",df.format(dvalue));
+            assertEquals(new Locale("fr","FR"), Locale.getDefault());
             assertEquals("123456123456.79",PerformanceStatisticUtil.format(dvalue));
             
             Locale.setDefault(new Locale("de","DE"));
-            df = new DecimalFormat(pattern);
-            assertEquals("123.456.123.456,79",df.format(dvalue));
+            assertEquals(new Locale("de","DE"), Locale.getDefault());
             assertEquals("123456123456.79",PerformanceStatisticUtil.format(dvalue));
-            
-            
-            DecimalFormat dformat = (DecimalFormat)NumberFormat.getNumberInstance(new Locale("en","US"));
-            assertEquals("123,456,123,456.789", dformat.format(dvalue));
-            assertEquals("123456123456.79", PerformanceStatisticUtil.format(dvalue));
-            
-            dformat = (DecimalFormat)NumberFormat.getNumberInstance(new Locale("fr","FR"));
-            assertEquals("123 456 123 456,789", dformat.format(dvalue));
-            assertEquals("123456123456.79", PerformanceStatisticUtil.format(dvalue));
-            
-            dformat = (DecimalFormat)NumberFormat.getNumberInstance(new Locale("de","DE"));
-            assertEquals("123.456.123.456,789", dformat.format(dvalue));
-            assertEquals("123456123456.79", PerformanceStatisticUtil.format(dvalue));
         } finally {
             Locale.setDefault(defaultLocale);
         }
