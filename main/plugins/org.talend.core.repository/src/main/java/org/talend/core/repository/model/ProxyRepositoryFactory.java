741de03c9a447ba81930422a3b4059a29808b9d2
jding-tlnd <jding@talend.com>
2021-01-25 19:01:25 +0800

    feat(TUP-29943): rename codejar should update package and folder path for inner code https://jira.talendforge.org/browse/TUP-29943
    Signed-off-by: jding-tlnd <jding@talend.com>

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
@@ -1352,6 +1352,12 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
     }
 
     @Override
+    public List<IRepositoryViewObject> getAll(Project project, ERepositoryObjectType type, boolean withDeleted,
+            boolean allVersions, IFolder... folders) throws PersistenceException {
+        return this.repositoryFactoryFromProvider.getAll(project, type, withDeleted, allVersions, folders);
+    }
+
+    @Override
     public List<IRepositoryViewObject> getAllCodesJars(ERepositoryObjectType type) throws PersistenceException {
         return getAllCodesJars(projectManager.getCurrentProject(), type);
     }
