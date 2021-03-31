6b05fc7ba0150bbb16ebf0a14241b1265e3f3362
sbliu <sbliu@talend.com>
2021-03-31 14:49:45 +0800

    fix(TUP-30768): move 'reset performance record' to other place.

--- main/plugins/org.talend.rcp/src/main/java/org/talend/rcp/intro/Application.java
+++ main/plugins/org.talend.rcp/src/main/java/org/talend/rcp/intro/Application.java
@@ -316,8 +316,6 @@ public class Application implements IApplication {
                 if (patchComponent.needRelaunch()) {
                     needRelaunch = true;
                 }
-                
-                afterInstallPatch();
             }
             if (StringUtils.isNotEmpty(patchComponent.getFailureMessage())) {
                 log.log(Level.ERROR, patchComponent.getFailureMessage());
@@ -339,8 +337,6 @@ public class Application implements IApplication {
                     if (installComponent.needRelaunch()) {
                         needRelaunch = true;
                     }
-                    
-                    afterInstallPatch();
                 }
                 if (StringUtils.isNotEmpty(installComponent.getFailureMessage())) {
                     log.log(Level.ERROR, installComponent.getFailureMessage());
@@ -361,10 +357,6 @@ public class Application implements IApplication {
         return needRelaunch;
     }
     
-    private void afterInstallPatch() {
-        TokenCollectorFactory.getFactory().reset();
-    }
-
     private void setRelaunchData() {
         EclipseCommandLine.updateOrCreateExitDataPropertyWithCommand(EclipseCommandLine.CLEAN, null, false, true);
         EclipseCommandLine.updateOrCreateExitDataPropertyWithCommand(EclipseCommandLine.TALEND_RELOAD_COMMAND,
