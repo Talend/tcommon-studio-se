d24b9f9fe53ad3da04f8c2d92a6dd30255470704
hzhao-talendbj <hzhao@talend.com>
2021-04-01 01:04:09 +0800

    TUP-30921 new code change

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/ContextOrderProperties.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/ContextOrderProperties.java
@@ -78,23 +78,12 @@ public class ContextOrderProperties extends Properties {
     @Override
     public Object put(Object key, Object value) {
         keys.add(key);
-        return super.put(key, value == null ? getDefaultValue(value) : value);
+        if (value == null) {
+            return super.remove(key);
+        }
+        return super.put(key, value);
     }
 
-    private Object getDefaultValue(Object value) {
-        if (value instanceof String) {
-            return "";
-        } else if (value instanceof Integer || value instanceof Byte || value instanceof Short || value instanceof Long) {
-            return 0;
-        } else if (value instanceof Double || value instanceof Float) {
-            return 0.0;
-        } else if (value instanceof Character) {
-            return Character.MIN_VALUE;
-        } else if (value instanceof Boolean) {
-            return false;
-        }
-        return value;
-    }
     @Override
     public synchronized Object remove(Object key) {
         keys.remove(key);
