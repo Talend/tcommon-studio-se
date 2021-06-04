e9e2dd4aa82eef5c688f201c8fcf06ad6b1aa0ce
sbliu <sbliu@talend.com>
2021-06-04 16:38:59 +0800

    fix(TUP-31252) add code to look amc preference.

--- main/plugins/org.talend.commons.runtime/src/org/talend/commons/runtime/service/ICollectDataService.java
+++ main/plugins/org.talend.commons.runtime/src/org/talend/commons/runtime/service/ICollectDataService.java
@@ -14,6 +14,17 @@ public interface ICollectDataService {
 
     final String KEY_SOURCE = "source";
 
+    //
+    final String AMC_FILE_TYPE_USED = "FILE_TYPE_USED";
+
+    final String AMC_DATABASE_TYPE_USED = "DATABASE_TYPE_USED";
+
+    final String AMC_PREVIEW_KEY = "amc.datasource";
+
+    final String AMC_PREVIEW_FILEVALUE = "File";
+
+    final String AMC_PREVIEW_DATABASEVALUE = "Database";
+
     /**
      * @return json string
      */
--- main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/token/AMCUsageTokenCollector.java
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/token/AMCUsageTokenCollector.java
@@ -14,6 +14,11 @@ package org.talend.core.ui.token;
 
 import java.util.Properties;
 
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.talend.commons.runtime.service.ICollectDataService;
 
 import us.monoid.json.JSONObject;
@@ -26,9 +31,25 @@ public class AMCUsageTokenCollector extends AbstractTokenCollector {
         ICollectDataService instance = ICollectDataService.getInstance("amc");
         if (instance != null) {
             props = instance.getCollectedData();
+        } else {
+            IScopeContext[] contexts = new IScopeContext[] { InstanceScope.INSTANCE, ConfigurationScope.INSTANCE,
+                    DefaultScope.INSTANCE };
+            String plugin = "org.talend.amc";
+            for (IScopeContext context : contexts) {
+                IEclipsePreferences amc = context.getNode(plugin);
+                if (amc != null) {
+                    if (amc.getBoolean(ICollectDataService.AMC_FILE_TYPE_USED, false)) {
+                        props.setProperty(ICollectDataService.AMC_PREVIEW_KEY, ICollectDataService.AMC_PREVIEW_FILEVALUE);
+                    } else if (amc.getBoolean(ICollectDataService.AMC_DATABASE_TYPE_USED, false)) {
+                        props.setProperty(ICollectDataService.AMC_PREVIEW_KEY, ICollectDataService.AMC_PREVIEW_DATABASEVALUE);
+                    }
+                    break;
+                }
+            }
+
         }
         JSONObject finalToken = new JSONObject();
-
+        finalToken.put(ICollectDataService.AMC_PREVIEW_KEY, "<Empty>");
         for (Object key : props.keySet()) {
             finalToken.put((String) key, props.get(key));
         }
